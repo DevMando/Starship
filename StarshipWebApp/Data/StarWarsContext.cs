c2b24d15566a8ef5e5363a516d4ade85c0d61763
Armando Fernandez <codefilez@gmail.com>
2025-06-27 11:07:43 -0700

    Fixed my DbContext class. I accidentally nesting the class with two dbcontext names 'StarWars' & 'StarWarsContext'.

--- StarshipWebApp/Data/StarWarsContext.cs
+++ StarshipWebApp/Data/StarWarsContext.cs
@@ -3,21 +3,18 @@ using StarshipWebApp.Models;
 
 namespace StarshipWebApp.Data
 {
-    public class StarWarsContext
+    public class StarWarsContext : DbContext
     {
-        public class StarWars : DbContext
+        public StarWarsContext(DbContextOptions<StarWarsContext> options) : base(options)
         {
-            public StarWars(DbContextOptions<StarWars> options) : base(options)
-            {
-            }
-
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
-            {
-                base.OnModelCreating(modelBuilder);
-            }
-
-
-            public DbSet<Starship> Starships { get; set; }
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<Starship> Starships { get; set; }
     }
 }
