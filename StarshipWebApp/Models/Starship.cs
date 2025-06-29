1bc9dfb0efea119e7a085ca0097eac6f82128662
Armando Fernandez <codefilez@gmail.com>
2025-06-29 16:06:00 -0700

    Implemented Starship Create Component with appropriate steps to create a new starship.

--- StarshipWebApp/Models/Starship.cs
+++ StarshipWebApp/Models/Starship.cs
@@ -70,6 +70,7 @@ namespace StarshipWebApp.Models
         public DateTime UpdatedDate { get; set; } = DateTime.Now;
 
         [JsonPropertyName("url")]
-        public string Url { get; set; }
+        public string? Url { get; set; } = null;
+
     }
 }
